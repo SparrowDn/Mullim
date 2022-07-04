d9595fb0ee48f5467a43a3a77327fe3508470519
SparrowDn <kidon.kinn@gmail.com>
2022-07-04 15:02:00 +0900

    .

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'SearchPage.dart';
+import 'addpage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -33,49 +34,65 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _bodywidget() {
-    return DefaultTabController(
-        length: 3,
-        child: CustomScrollView(
-          slivers: [
+    return Scaffold(
+      body: Container(
+        child: DefaultTabController(
+          length: 3,
+          child: CustomScrollView(
+            slivers: [
+              // 상단의 앱바 제목표시
 
-            // 상단의 앱바 제목표시
+              // const SliverAppBar(
+              //   title: Text(
+              //     "음뭐로할까",
+              //     style: TextStyle(color: Colors.black),
+              //   ),
+              //   pinned: false,
+              //   backgroundColor: Colors.white,
+              // ),
+              const SliverPersistentHeader(delegate: TabBarDelegate()),
 
-            // const SliverAppBar(
-            //   title: Text(
-            //     "음뭐로할까",
-            //     style: TextStyle(color: Colors.black),
-            //   ),
-            //   pinned: false,
-            //   backgroundColor: Colors.white,
-            // ),
-            const SliverPersistentHeader(delegate: TabBarDelegate()),
+              //탭 내부의 내용 표시하는 탭
+              SliverFillRemaining(
+                hasScrollBody: true,
+                child: TabBarView(
+                  children: [
+                    Container(
+                      color: Colors.white,
+                    ),
+                    Container(
+                      color: Colors.white,
+                    ),
+                    Container(
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => addpage()));
+        },
+        backgroundColor: Colors.blue,
+        child: Icon(Icons.add),
 
-            //탭 내부의 내용 표시하는 탭
-            SliverFillRemaining(
-              hasScrollBody: true,
-              child: TabBarView(children: [
-                Container(
-                  color: Colors.amber,
-                ),
-                Container(
-                  color: Colors.black,
-                ),
-                Container(
-                  color: Colors.orange,
-                ),
-              ]),
-            )
-          ],
-        ));
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: _appbarwidget(),
-    body: _bodywidget(),);
+    return Scaffold(
+      appBar: _appbarwidget(),
+      body: _bodywidget(),
+    );
   }
 }
-
 
 //탭바 상단의 탭 항목, 이름 선택시 색상
 class TabBarDelegate extends SliverPersistentHeaderDelegate {
@@ -89,19 +106,28 @@ class TabBarDelegate extends SliverPersistentHeaderDelegate {
         tabs: [
           Tab(
             child: Container(
-              child: Text("홈", style: TextStyle(color: Colors.black),),
+              child: Text(
+                "홈",
+                style: TextStyle(color: Colors.black),
+              ),
               color: Colors.white,
             ),
           ),
           Tab(
             child: Container(
-              child: Text("페이지", style: TextStyle(color: Colors.black),),
+              child: Text(
+                "페이지",
+                style: TextStyle(color: Colors.black),
+              ),
               color: Colors.white,
             ),
           ),
           Tab(
             child: Container(
-              child: Text("추천", style: TextStyle(color: Colors.black),),
+              child: Text(
+                "추천",
+                style: TextStyle(color: Colors.black),
+              ),
               color: Colors.white,
             ),
           ),
@@ -111,7 +137,6 @@ class TabBarDelegate extends SliverPersistentHeaderDelegate {
         indicatorColor: Colors.black,
         indicatorSize: TabBarIndicatorSize.tab,
         labelColor: Colors.black,
-
       ),
     );
     // TODO: implement build
---
+++ lib/pages/addpage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class addpage extends StatefulWidget {
+  const addpage({Key? key}) : super(key: key);
+
+  @override
+  State<addpage> createState() => _addpageState();
+}
+
+class _addpageState extends State<addpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
