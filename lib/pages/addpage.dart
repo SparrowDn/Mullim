b5252c3c2926d41468f0877de94c7c6074335420
Sparrowdnx <kidon.kinn@gmail.com>
2022-10-27 04:00:55 +0900

    221027

--- lib/pages/addpage.dart
+++ lib/pages/addpage.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:mullim/utils/crud.dart';
+import 'package:image_picker/image_picker.dart';
 
 import 'choicepage.dart';
 
@@ -9,11 +10,7 @@ class addpage extends StatefulWidget {
 
   @override
   State<addpage> createState() => _addpageState();
-
 }
-
-
-
 
 class _addpageState extends State<addpage> {
   TextEditingController titlecontroller = TextEditingController();
@@ -27,7 +24,6 @@ class _addpageState extends State<addpage> {
     '항공정비과',
   ];
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,8 +31,6 @@ class _addpageState extends State<addpage> {
       body: _bodywidget(),
     );
   }
-
-
 
   //앱바위젯
   PreferredSizeWidget _appbarwidget() {
@@ -61,7 +55,6 @@ class _addpageState extends State<addpage> {
 
   //바디
   _bodywidget() {
-
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -91,76 +84,109 @@ class _addpageState extends State<addpage> {
                 ),
               ),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 6),
 
-
-            //가격 입력 칸
+            //가격, 과 입력 칸
             Container(
               //color: Colors.blue,
               alignment: Alignment.centerLeft,
               padding: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
               child: Container(
-                child: Expanded(
-                  child: Container(
-                    child: TextFormField(
-                      controller: pricecontroller,
-                      keyboardType: TextInputType.number,
-                      validator: (value) {
-                        if (value == null || value.trim().isEmpty) {
-                          return '필수 항목입니다';
-                        }
-                      },
-                      decoration: InputDecoration(
-                        hintText: '희망 가격',
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black12),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 190,
+                      height: 72,
+                      child: Column(
+                        children: [
+                          SizedBox(height: 20),
+                          DropdownButton(
+                            isExpanded: true,
+                            hint: Text("과를 선택하세요!"),
+
+                            // Initial Value
+                            value: dropdownvalue,
+
+                            // Down Arrow Icon
+                            icon: const Icon(Icons.keyboard_arrow_down),
+
+                            // Array list of items
+                            items: items.map((String items) {
+                              return DropdownMenuItem(
+                                value: items,
+                                child: Text(items),
+                              );
+                            }).toList(),
+                            // After selecting the desired option,it will
+                            // change button value to selected value
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                dropdownvalue = newValue!;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      child: Expanded(
+                        child: TextFormField(
+                          controller: pricecontroller,
+                          keyboardType: TextInputType.number,
+                          validator: (value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return '필수 항목입니다';
+                            }
+                          },
+                          decoration: InputDecoration(
+                            hintText: '희망 가격',
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black12),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
               ),
             ),
             SizedBox(height: 20),
 
-            //과 선택 창
             Container(
-              child:
-              DropdownButton(
-
-                hint: Text("과를 선택하세요!"),
-
-                // Initial Value
-                value: dropdownvalue,
-
-                // Down Arrow Icon
-                icon: const Icon(Icons.keyboard_arrow_down),
-
-                // Array list of items
-                items: items.map((String items) {
-                  return DropdownMenuItem(
-                    value: items,
-                    child: Text(items),
-                  );
-                }).toList(),
-                // After selecting the desired option,it will
-                // change button value to selected value
-                onChanged: (String? newValue) {
-                  setState(() {
-                    dropdownvalue = newValue!;
-                  });
-                },
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+              child: TextField(
+                decoration: const InputDecoration(
+                  hintText: "설명을 입력하세요!",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black12),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+                keyboardType: TextInputType.multiline,
+                minLines: 1, //Normal textInputField will be displayed
+                maxLines: 5, // when user presses enter it will adapt to it
               ),
             ),
+
+            //사진파일
+            Container(
+
+            ),
+
+
 
           ],
         ),
       ),
     );
   }
-
-
 }
